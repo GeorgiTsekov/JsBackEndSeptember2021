e17e95fc7b0127e53bc623d246c9be93157e5cfc
Georgi Tsekov <georgi_tsekov@abv.bg>
2021-09-23 16:27:12 +0300

    cat-shelter continue

--- cat-shelter/index.js
+++ cat-shelter/index.js
@@ -1,5 +1,8 @@
 const http = require('http');
 const fs = require('fs');
+const formidable = require('formidable');
+
+const storageService = require('./services/storageService.js');
 
 const app = http.createServer((req, res) => {
     switch (req.url) {
@@ -28,17 +31,53 @@ const app = http.createServer((req, res) => {
             res.end();
             break;
         case '/cats/add-cat':
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-            fs.readFile('./views/addCat.html', (err, result) => {
-                if (err) {
-                    res.statusCode = 404;
-                    return res.end();
-                }
-                res.write(result);
-                res.end();
-            });
+            if (req.method == 'GET') {
+                res.writeHead(200, {
+                    'Content-Type': 'text/html'
+                });
+                fs.readFile('./views/addCat.html', 'utf8', (err, result) => {
+                    if (err) {
+                        res.statusCode = 404;
+                        return res.end();
+                    }
+
+                    let breeds = [
+                        'Persian',
+                        'Angora',
+                        'StreetBeauty',
+                    ];
+
+                    let mappedBreeds = breeds.map(x => `
+                        <option value="${x}">${x}</option>
+                    `)
+
+                    result = result.replace('{{breeds}}', mappedBreeds);
+
+                    res.write(result);
+                    res.end();
+                });
+            } else if (req.method == 'POST') {
+                let form = formidable.IncomingForm();
+
+                form.parse(req, (err, fields, files) => {
+
+                    console.log(fields);
+                    console.log(files);
+                    storageService.saveCat(fields)
+                        .then(() => {
+                            console.log('end');
+                            res.end();
+                        })
+                        .catch(err => {
+                            console.log('err');
+                            console.log(err);
+                        })
+
+                    res.writeHead(302, {
+                        'Location': '/'
+                    })
+                });
+            }
             break;
         default:
             res.statusCode = 404;
