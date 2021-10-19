3e8f3ecc5296c06c2a05cd121b920e020a8cdfa6
Georgi Tsekov <georgi_tsekov@abv.bg>
2021-10-19 19:59:00 +0300

    add User model, database mongoose, authService, utils with jwt token, add login register work with authService

--- exam-prep/real-estate-agency/src/controllers/authController.js
+++ exam-prep/real-estate-agency/src/controllers/authController.js
@@ -1,11 +1,56 @@
 const router = require('express').Router();
 
+const authService = require('../services/authService');
+const { AUTH_COOKIE_NAME } = require('../constants')
+
 router.get('/login', (req, res) => {
     res.render('auth/login');
 });
 
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        let token = await authService.login({
+            username,
+            password,
+        });
+
+        res.cookie(AUTH_COOKIE_NAME, token);
+        
+        res.redirect('/');
+    } catch (error) {
+        // return error
+        console.log(error);
+        res.end();
+    }
+});
+
 router.get('/register', (req, res) => {
     res.render('auth/register');
+});
+
+router.post('/register', async (req, res) => {
+    const { name, username, password, rePassword } = req.body;
+
+    if (password !== rePassword) {
+        res.locals.error = 'Password missmatch';
+
+        return res.render('auth/register');
+    }
+
+    try {
+        await authService.register({
+            name,
+            username,
+            password,
+        });
+
+        res.redirect('/');
+
+    } catch (error) {
+        // return error
+    }
 });
 
 module.exports = router;
