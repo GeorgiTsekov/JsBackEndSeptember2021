f162240e657b742c7a16e28307ae721d947b0e45
Georgi Tsekov <georgi_tsekov@abv.bg>
2021-11-01 23:13:00 +0200

    add login register routes, userController, userService to furniture-app

--- furniture-app/api/src/server.js
+++ furniture-app/api/src/server.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.json({text: 'It\'s working!'});
+mongoose.connect('mongodb://localhost:27017/furniture')
+    .then(() => {
+        console.log('DB Connected');
+    });
+
+mongoose.connection.on('error', (error) => {
+    console.log('DB Error: ', error);
 });
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.json({ text: 'It\'s working!' });
+});
+
+app.use(routes);
 
 app.listen(3030, console.log('App is running on port 3030'));
